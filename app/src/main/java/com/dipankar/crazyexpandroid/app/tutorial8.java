0d20f4e6848020bc15354b7086f0049816e3c3d1
ddutta <d@d>
2015-04-17 17:17:16 +0530

    Pre Projects

---
+++ app/src/main/java/com/dipankar/crazyexpandroid/app/tutorial8.java
@@ -0,0 +1,13 @@
+package com.dipankar.crazyexpandroid.app;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class tutorial8 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial8);
+    }
+}
