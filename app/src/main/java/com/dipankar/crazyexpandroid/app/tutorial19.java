6db9b81852b5de30dea71b19a8abf57c811ad46b
dipankar08 <dutta.dipankar08@gmail.com>
2015-04-27 03:30:18 +0530

    new work

--- app/src/main/java/com/dipankar/crazyexpandroid/app/tutorial19.java
+++ app/src/main/java/com/dipankar/crazyexpandroid/app/tutorial19.java
@@ -1,1 +1,58 @@
-//TBD
+package com.dipankar.crazyexpandroid.app;// Auto start using alerm Manager
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.view.View;
+import android.widget.Toast;
+
+public class tutorial19 extends Activity {
+    private tutorial19_AlarmManagerBroadcastReceiver alarm;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial19);
+        alarm = new tutorial19_AlarmManagerBroadcastReceiver();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    public void startRepeatingTimer(View view) {
+        Context context = this.getApplicationContext();
+        if(alarm != null){
+            alarm.SetAlarm(context);
+        }else{
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void cancelRepeatingTimer(View view){
+        Context context = this.getApplicationContext();
+        if(alarm != null){
+            alarm.CancelAlarm(context);
+        }else{
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onetimeTimer(View view){
+        Context context = this.getApplicationContext();
+        if(alarm != null){
+            alarm.setOnetimeTimer(context);
+        }else{
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
