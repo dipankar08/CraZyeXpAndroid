bcbb21769e02323838c3faffee07a06943678897
ddutta <ddutta@microsodt.com>
2015-08-25 16:10:03 +0530

    Tap Attack

--- app/src/main/java/com/dipankar/crazyexpandroid/app/tutorial34.java
+++ app/src/main/java/com/dipankar/crazyexpandroid/app/tutorial34.java
@@ -1,6 +1,8 @@
 /* This is a Templete You should Copy this to crete a new one... ha ha ha */
 package com.dipankar.crazyexpandroid.app;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -11,10 +13,69 @@ public class tutorial34 extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tutorial34);
         Toast.makeText(getApplicationContext(), "----VE clicked", Toast.LENGTH_LONG).show();
     }
     public void executeHello(View v) {
-        Toast.makeText(this, "Hello World clicked...", Toast.LENGTH_LONG).show();
+        showSimpleDialog();
     }
+    /* Ok and Cacle Button */
+    public void showSimpleDialog() {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(tutorial34.this);
+
+        // set title
+
+        alertDialogBuilder.setTitle("This is title");
+       // alertDialogBuilder.setIcon(R.drawable.sample);
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("This is the message")
+                .setCancelable(false)
+                .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, close
+                        // current activity
+                        tutorial34.this.finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show it
+        alertDialog.show();
+    }
+    /* Ok Button Only */
+    public void getOkDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(tutorial34.this);        // set title
+
+        alertDialogBuilder.setTitle("Invisible Activity found! ");
+        // alertDialogBuilder.setIcon(R.drawable.sample);
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("This is the message")
+                .setCancelable(false)
+        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // if this button is clicked, close
+                // current activity
+                tutorial34.this.finish();
+            }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show it
+        alertDialog.show();
+    }
+
 }
