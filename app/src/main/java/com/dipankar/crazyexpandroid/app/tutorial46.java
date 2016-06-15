d991cb08c5e850caa76786b145c5ab26a2e9a02d
ddutta <ddutt@mict.com>
2016-06-15 17:14:40 +0530

    Adding more placeholder

---
+++ app/src/main/java/com/dipankar/crazyexpandroid/app/tutorial46.java
@@ -0,0 +1,22 @@
+/* This is a Templete You should Copy this to crete a new one... ha ha ha */
+package com.dipankar.crazyexpandroid.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class tutorial46 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutorial46);
+        Toast.makeText(getApplicationContext(), "----VE clicked", Toast.LENGTH_LONG).show();
+    }
+    public void executeHello(View v) {
+        Toast.makeText(this, "Hello World clicked...", Toast.LENGTH_LONG).show();
+    }
+}
