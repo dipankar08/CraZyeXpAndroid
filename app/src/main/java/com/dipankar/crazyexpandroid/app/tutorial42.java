6e118c6998d074c5cd6d648ce8fa1b3e31058812
ddutta <ddutt@mict.com>
2016-06-15 20:11:30 +0530

    Adding more placeholder

--- app/src/main/java/com/dipankar/crazyexpandroid/app/tutorial42.java
+++ app/src/main/java/com/dipankar/crazyexpandroid/app/tutorial42.java
@@ -1,4 +1,4 @@
-/* This is a Templete You should Copy this to crete a new one... ha ha ha */
+/* View Filler*/
 package com.dipankar.crazyexpandroid.app;
 
 import android.support.v7.app.ActionBarActivity;
@@ -7,16 +7,80 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.widget.ViewFlipper;
 
 public class tutorial42 extends ActionBarActivity {
-
+    private ViewFlipper TruitonFlipper;
+    private float initialX;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tutorial42);
         Toast.makeText(getApplicationContext(), "----VE clicked", Toast.LENGTH_LONG).show();
+        TruitonFlipper = (ViewFlipper) findViewById(R.id.flipper);
+        TruitonFlipper.setInAnimation(this, android.R.anim.fade_in);
+        TruitonFlipper.setOutAnimation(this, android.R.anim.fade_out);
     }
-    public void executeHello(View v) {
-        Toast.makeText(this, "Hello World clicked...", Toast.LENGTH_LONG).show();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                initialX = touchevent.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                float finalX = touchevent.getX();
+                if (initialX > finalX) {
+                    if (TruitonFlipper.getDisplayedChild() == 1)
+                        break;
+
+				/*TruitonFlipper.setInAnimation(this, R.anim.in_right);
+				TruitonFlipper.setOutAnimation(this, R.anim.out_left);*/
+
+                    TruitonFlipper.showNext();
+                } else {
+                    if (TruitonFlipper.getDisplayedChild() == 0)
+                        break;
+
+				/*TruitonFlipper.setInAnimation(this, R.anim.in_left);
+				TruitonFlipper.setOutAnimation(this, R.anim.out_right);*/
+
+                    TruitonFlipper.showPrevious();
+                }
+                break;
+        }
+        return false;
+    }
+    // This snippet hides the system bars.
+    private void hideSystemUI() {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        mDecorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    // This snippet shows the system bars. It does this by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private void showSystemUI() {
+        mDecorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
