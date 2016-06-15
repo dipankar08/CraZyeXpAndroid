8b79a8ff7c360d17e0323babb624ecdeebf06837
ddutta <ddutt@mict.com>
2016-06-15 18:16:49 +0530

    Adding more placeholder

--- app/src/main/java/com/dipankar/crazyexpandroid/app/tutorial41.java
+++ app/src/main/java/com/dipankar/crazyexpandroid/app/tutorial41.java
@@ -3,13 +3,16 @@ package com.dipankar.crazyexpandroid.app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
-public class tutorial41 extends ActionBarActivity {
-
+public class tutorial41 extends ActionBarActivity implements View.OnTouchListener {
+    int i =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,4 +22,42 @@ public class tutorial41 extends ActionBarActivity {
     public void executeHello(View v) {
         Toast.makeText(this, "Hello World clicked...", Toast.LENGTH_LONG).show();
     }
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            Log.w("onKeyDown", "LONG KEYCODE_VOLUME_DOWN:"+(i++));
+            //Note : You must return false always to indicate the solund voluem echnages
+            return false;
+        }
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            Log.w("onKeyDown", "LONG KEYCODE_VOLUME_UP:"+(i++));
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {
+            Log.w("onKeyDown", "KEYCODE_VOLUME_DOWN:" + (i++));
+            event.startTracking();
+            return true;
+        }
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            Log.w("onKeyDown", "KEYCODE_VOLUME_UP:"+(i++));
+
+            event.startTracking();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
 }
+/**************************
+ * Notes:
+ *  1. Return false to do the default work like raise the sound
+ ******************************/
